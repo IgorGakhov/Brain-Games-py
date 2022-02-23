5ce09413c53a02fffc7bcbb07fa20f8eb11b34ff
Igor_Gakhov <gakhov_igor@rambler.ru>
2022-02-23 21:50:06 +0300

    Change the end of the cycle in the engine

--- brain_games/engine/game_engine.py
+++ brain_games/engine/game_engine.py
@@ -14,14 +14,10 @@ def true_answer(game_round, name):
     if game_round == GAME_ATTEMPTS:
         print(VICTORY_GAME_MESSAGE.format(name))
 
-    return True
-
 
 def false_answer(user_answer, target_result, name):
     # Исполняется, если последний введенный ответ - неправильный
     print(FALSE_ANSWER.format(str(user_answer), str(target_result), name))
-
-    return False
 
 
 def run_game(game_rules, generate_game_data, answer_is_correct):
@@ -30,9 +26,9 @@ def run_game(game_rules, generate_game_data, answer_is_correct):
     print(game_rules)
 
     # Формируем логику игры и генерируем цикл раундов
-    user_answer = True
+
     game_round = 1
-    while user_answer is True and game_round <= GAME_ATTEMPTS:
+    while game_round <= GAME_ATTEMPTS:
         random_game_value, computer_question = generate_game_data()
         print('Question: {}'.format(computer_question))
         user_answer = prompt.string('Your answer: ')
@@ -43,9 +39,9 @@ def run_game(game_rules, generate_game_data, answer_is_correct):
 
         # Узнаем правильность ответа
         # и в зависимости от нее вызываем функцию
-        if bool_result is True:
-            user_answer = true_answer(game_round, name)
-        else:
-            user_answer = false_answer(user_answer, target_result, name)
-
+        if not bool_result:
+            false_answer(user_answer, target_result, name)
+            break
+        
+        true_answer(game_round, name)
         game_round += 1
